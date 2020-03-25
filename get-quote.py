59710f4dfd08448668d8c1c60da4ca256f788cff
willfrid boris <boriskwayep35@yahoo.com>
2020-03-25 15:28:51 -0400

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -5,7 +5,8 @@ def program_python():
    f = open("quotes.txt")
    quotes = f.readlines()
    f.close()
-   last = len(quotes)-1
+   #last = len(quotes)-1
+   last=13
    rnd = random.randint(0, last)
 
    print(quotes[rnd])
